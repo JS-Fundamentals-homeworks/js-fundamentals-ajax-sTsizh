7ca62768a9956a5f41e6fd0d4fb8d0b5995913a7
sTsizh <sofiatsizh@gmail.com>
2025-08-08 15:18:32 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -5,3 +5,14 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+let usersList = document.querySelector('ul.usersList')
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+        for(let user of users){
+            let newElem = document.createElement('li')
+            newElem.innerHTML = user.name
+            usersList.append(newElem)
+        }
+    })
