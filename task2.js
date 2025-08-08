5b711c905c9c55fc619c995ecc199d75ae55c82f
sTsizh <sofiatsizh@gmail.com>
2025-08-08 15:18:57 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -5,3 +5,25 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+let getUserButton = document.getElementById('getUserButton')
+let userCity = document.getElementById('userCity')
+
+function getUser(event){
+    event.preventDefault()
+    let userNameInputValue = document.getElementById('userNameInput').value
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+        for(let user of users){
+            if(user.username == userNameInputValue){
+                userCity.innerHTML = user.address.city
+                return
+             } else{
+                userCity.innerHTML = 'No user found!'
+            }
+        }
+    })
+}
+
+getUserButton.addEventListener('click', getUser)
